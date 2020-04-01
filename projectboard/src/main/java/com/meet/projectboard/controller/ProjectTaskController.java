be85606705002f9d024ad36dba7c324df3b1ee1d
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-01 19:14:10 +0530

    Created [post] method for creating project task

--- projectboard/src/main/java/com/meet/projectboard/controller/ProjectTaskController.java
+++ projectboard/src/main/java/com/meet/projectboard/controller/ProjectTaskController.java
@@ -1,12 +1,48 @@
 package com.meet.projectboard.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.meet.projectboard.model.ProjectTask;
+import com.meet.projectboard.service.ProjectTaskService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/projectboard")
 public class ProjectTaskController {
+
+	@Autowired
+	private ProjectTaskService projectTaskService;
+	
+	@PostMapping("")
+	private ResponseEntity<?> addProjectTask(@Valid @RequestBody ProjectTask projectTask, BindingResult bindingResult){
+		
+		if(bindingResult.hasErrors()) {
+			
+			Map<String, String> errorMap = new HashMap<String,String>();
+			
+			for(FieldError fieldError : bindingResult.getFieldErrors()) {
+				errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+		}
+		
+		ProjectTask newProjectTask = projectTaskService.saveOrUpdateProjectTask(projectTask);
+		
+		return new ResponseEntity<ProjectTask>(newProjectTask,HttpStatus.OK);
+	}
 
 }
--- projectboard/src/main/java/com/meet/projectboard/service/ProjectTaskService.java
+++ projectboard/src/main/java/com/meet/projectboard/service/ProjectTaskService.java
@@ -1,9 +1,25 @@
 package com.meet.projectboard.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.meet.projectboard.model.ProjectTask;
+import com.meet.projectboard.repository.ProjectTaskRepository;
 
 @Service
 public class ProjectTaskService {
+
+	@Autowired
+	private ProjectTaskRepository projectTaskRepository;
+
+	public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+		
+		if(projectTask.getStatus() == null || projectTask.getStatus() == "") {
+			projectTask.setStatus("TO_DO");
+		}
+		projectTaskRepository.save(projectTask);
+		return projectTask;
+	}
 
 	
 }
