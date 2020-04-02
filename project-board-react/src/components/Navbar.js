49dc1869dc7c1a7102be318a4ce3c5b757d3ec58
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-02 13:01:56 +0530

    Created navbar component

---
+++ project-board-react/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+                <div class="container">
+                    <a class="navbar-brand" href="Dashboard.html">
+                    Project Task Tool
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+                    <span class="navbar-toggler-icon" />
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
+}
