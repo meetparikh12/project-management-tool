ff06deed400084c5deec7fce3451b52c0c77204d
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-02 18:44:53 +0530

    Converted AddProjectTask component to Controlled component

--- project-board-react/src/components/ProjectTask/AddProjectTask.js
+++ project-board-react/src/components/ProjectTask/AddProjectTask.js
@@ -3,6 +3,28 @@ import {Link} from 'react-router-dom';
 
 
 export default class AddProjectTask extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            summary: "",
+            acceptanceCriteria: "",
+            status: ""
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+    }
+
+    onChange(e){
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmitForm(e){
+        e.preventDefault();
+        console.log(this.state);
+    }
     render() {
         return (
             <div>
@@ -15,20 +37,23 @@ export default class AddProjectTask extends Component {
                                 <h4 className="display-4 text-center">Add /Update Project Task</h4>
                                 <form>
                                     <div className="form-group">
-                                        <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary" />
+                                        <input type="text" className="form-control form-control-lg" name="summary" 
+                                        value={this.state.summary} placeholder="Project Task summary" onChange={this.onChange}/>
                                     </div>
                                     <div className="form-group">
-                                        <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                                        <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" 
+                                        value={this.state.acceptanceCriteria} name="acceptanceCriteria" onChange={this.onChange}></textarea>
                                     </div>
                                     <div className="form-group">
-                                        <select className="form-control form-control-lg" name="status">
+                                        <select className="form-control form-control-lg" value={this.state.status} onChange={this.onChange} 
+                                        name="status">
                                             <option value="">Select Status</option>
                                             <option value="TO_DO">TO DO</option>
                                             <option value="IN_PROGRESS">IN PROGRESS</option>
                                             <option value="DONE">DONE</option>
                                         </select>
                                     </div>
-                                    <input type="submit" className="btn btn-primary btn-block mt-4" />
+                                    <input type="submit" onClick={this.onSubmitForm} className="btn btn-primary btn-block mt-4" />
                                 </form>
                             </div>
                         </div>
