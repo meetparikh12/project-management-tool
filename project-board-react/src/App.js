7f2600b93285cfcb7ca2f03b24ef9d766f989abc
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-02 16:12:14 +0530

    Created ProjectBoard component

--- project-board-react/src/App.js
+++ project-board-react/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import ProjectBoard from './components/ProjectBoard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
+        <ProjectBoard/>
       </div>
     )
   }
