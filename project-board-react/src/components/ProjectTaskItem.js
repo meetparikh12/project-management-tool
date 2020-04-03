69d06566dc568695e8e3d7776d9c4c807a40e2c8
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-04 00:44:06 +0530

    Restructing of ProjectTaskItem component

--- project-board-react/src/components/ProjectTaskItem.js
+++ project-board-react/src/components/ProjectTaskItem.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 
 export default class ProjectTaskItem extends Component {
     render() {
+        const { project_task } = this.props;
         return (
             <div>
                 <div className="card mb-1 bg-light">
 
                     <div className="card-header text-primary">
-                        ID: {this.props.project_task.id}
+                        ID: {project_task.id}
                     </div>
                     
                     <div className="card-body bg-light">
-                        <h5 className="card-title">{this.props.project_task.summary}</h5>
+                        <h5 className="card-title">{project_task.summary}</h5>
                         <p className="card-text text-truncate ">
-                            {this.props.project_task.acceptanceCriteria}
+                            {project_task.acceptanceCriteria}
                         </p>
                         <a href="" className="btn btn-primary">
                             View / Update
