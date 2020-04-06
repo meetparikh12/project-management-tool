0b4ca39674d20e489644b65ed0219331b4afb425
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-06 18:45:23 +0530

    Added and handled validations for Project Entity

--- projectboard/src/main/java/com/meet/projectboard/controller/ProjectController.java
+++ projectboard/src/main/java/com/meet/projectboard/controller/ProjectController.java
@@ -1,10 +1,15 @@
 package com.meet.projectboard.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +28,18 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("/project")
-	private ResponseEntity<?> addProject(@Valid @RequestBody Project project){
+	private ResponseEntity<?> addProject(@Valid @RequestBody Project project, BindingResult bindingResult){
+		
+		if(bindingResult.hasErrors()) {
+			
+			Map<String,String> errorMap = new HashMap<String,String>();
+			
+			for (FieldError fieldError: bindingResult.getFieldErrors()) {
+				errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+		
+			return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+		}
 		
 		Project newProject = projectService.saveOrUpdateProject(project);
 		
