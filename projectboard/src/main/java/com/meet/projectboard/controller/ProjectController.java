0d942212b9eae69139566edb2be8b573212c128f
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-06 19:45:38 +0530

    Added validations to Project Entity fo creating Project and created a seperate service for handling errors

--- projectboard/src/main/java/com/meet/projectboard/controller/ProjectController.java
+++ projectboard/src/main/java/com/meet/projectboard/controller/ProjectController.java
@@ -1,15 +1,11 @@
 package com.meet.projectboard.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meet.projectboard.model.Project;
+import com.meet.projectboard.service.MapValidationErrorService;
 import com.meet.projectboard.service.ProjectService;
 
 @RestController
@@ -27,22 +24,16 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("/project")
 	private ResponseEntity<?> addProject(@Valid @RequestBody Project project, BindingResult bindingResult){
 		
-		if(bindingResult.hasErrors()) {
-			
-			Map<String,String> errorMap = new HashMap<String,String>();
-			
-			for (FieldError fieldError: bindingResult.getFieldErrors()) {
-				errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
-			}
-		
-			return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(bindingResult);
+		if(errorMap != null) return errorMap;
 		
 		Project newProject = projectService.saveOrUpdateProject(project);
-		
 		return new ResponseEntity<Project>(newProject,HttpStatus.OK);
 	}
 }
--- projectboard/src/main/java/com/meet/projectboard/model/Project.java
+++ projectboard/src/main/java/com/meet/projectboard/model/Project.java
@@ -3,6 +3,7 @@ package com.meet.projectboard.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Table(name="project")
 @Entity
@@ -24,22 +28,29 @@ public class Project {
 	private String projectName; 
 	
 	@NotBlank(message="Project code is required")
+	@Size(min=4, max=5, message="Project code must be between 4 to 5 characters.")
+	@Column(updatable = false , unique = true)
 	private String projectIdentifier;
 	
 	@NotBlank(message="Project description is required")
 	private String projectDescription;
 	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date startDate;
-	
+
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date endDate;
 	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date createdAt;
 	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date updatedAt;
 	
 	@PrePersist
 	protected void onCreate() {
 		this.createdAt = new Date();
+		this.updatedAt = new Date();
 	}
 	
 	@PreUpdate
