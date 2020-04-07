b8f8442cb3c08883fa553cf58eb944e5d8d61703
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-07 21:13:19 +0530

    Converted AddProject component to Controlled form Component

--- project-board-react/src/components/Project/AddProject.js
+++ project-board-react/src/components/Project/AddProject.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class AddProject extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            projectName: '',
+            projectIdentifier: '',
+            projectDescription: '',
+            start_date: '',
+            end_date: ''
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    
+    onChange(e){
+
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        const newProject = {
+            "projectName" : this.state.projectName,
+            "projectIdentifier" : this.state.projectIdentifier,
+            "projectDescription" : this.state.projectDescription,
+            "startDate" : this.state.start_date,
+            "endDate" : this.state.end_date
+        }
+        console.log(newProject);
+    }
+
     render() {
         return (
             <div>
@@ -10,24 +43,24 @@ class AddProject extends Component {
                             <div className="col-md-8 m-auto">
                                 <h5 className="display-4 text-center">Create / Edit Project form</h5>
                                 <hr />
-                                <form>
+                                <form onSubmit={this.onSubmit}>
                                     <div className="form-group">
-                                        <input type="text" className="form-control form-control-lg " placeholder="Project Name" />
+                                        <input type="text" className="form-control form-control-lg " name="projectName" onChange={this.onChange} value={this.state.projectName} placeholder="Project Name" />
                                     </div>
                                     <div className="form-group">
-                                        <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID"
+                                        <input type="text" className="form-control form-control-lg" name="projectIdentifier" onChange={this.onChange} value={this.state.projectIdentifier} placeholder="Unique Project ID"
                                              />
                                     </div>
                                     <div className="form-group">
-                                        <textarea className="form-control form-control-lg" placeholder="Project Description"></textarea>
+                                        <textarea className="form-control form-control-lg" name="projectDescription" onChange={this.onChange} value={this.state.projectDescription} placeholder="Project Description"></textarea>
                                     </div>
                                     <h6>Start Date</h6>
                                     <div className="form-group">
-                                        <input type="date" className="form-control form-control-lg" name="start_date" />
+                                        <input type="date" className="form-control form-control-lg" name="start_date" onChange={this.onChange} value={this.state.start_date}/>
                                     </div>
                                     <h6>Estimated End Date</h6>
                                     <div className="form-group">
-                                        <input type="date" className="form-control form-control-lg" name="end_date" />
+                                        <input type="date" className="form-control form-control-lg" name="end_date" onChange={this.onChange} value={this.state.end_date}/>
                                     </div>
 
                                     <input type="submit" className="btn btn-primary btn-block mt-4" />
