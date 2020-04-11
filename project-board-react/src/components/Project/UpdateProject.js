fee6b9f4fac1f9c6c7aba884a2d2db1f3e13ee39
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-12 00:04:16 +0530

    Converted UpdateProject Component to Controlled Component

--- project-board-react/src/components/Project/UpdateProject.js
+++ project-board-react/src/components/Project/UpdateProject.js
@@ -4,8 +4,37 @@ import PropTypes from 'prop-types';
 
 class UpdateProject extends Component {
     
+    constructor(props){
+        super(props);
+        this.state = {
+            start_date:"",
+            end_date:"",
+            project_name:"",
+            project_description:""
+        }
+        this.onChange = this.onChange.bind(this);
+   //     this.onSubmit = this.onSubmit.bind(this);
+    }
+    
+    componentWillReceiveProps(nextProps){
+        if(nextProps.current_project)
+        this.setState({
+            project_name: nextProps.current_project.projectName,
+            project_description: nextProps.current_project.projectDescription,
+            start_date: nextProps.current_project.startDate,
+            end_date: nextProps.current_project.endDate
+        })
+    }
+
+    onChange(e){
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+
     render() {
-        const { currentProject } = this.props;
+        const { current_project } = this.props;
         return (
             <div className="UpdateProject">
                 <div className="project">
@@ -16,22 +45,27 @@ class UpdateProject extends Component {
                                 <hr />
                                 <form>
                                     <div className="form-group">
-                                        <input type="text" className="form-control form-control-lg " value = {currentProject.projectName} placeholder="Project Name" />
+                                        <input type="text" className="form-control form-control-lg " 
+                                        name="project_name" onChange = {this.onChange} value = {this.state.project_name} placeholder="Project Name" />
                                     </div>
                                     <div className="form-group">
-                                        <input type="text" className="form-control form-control-lg" value={currentProject.projectIdentifier} 
+                                        <input type="text" className="form-control form-control-lg" 
+                                        name="projectIdentifier" value={current_project.projectIdentifier} 
                                         placeholder="Unique Project ID" disabled />
                                     </div>
                                     <div className="form-group">
-                                        <textarea className="form-control form-control-lg" value = {currentProject.projectDescription} placeholder="Project Description"></textarea>
+                                        <textarea className="form-control form-control-lg" 
+                                        name="project_description"  onChange = {this.onChange}  value = {this.state.project_description} placeholder="Project Description"></textarea>
                                     </div>
                                     <h6>Start Date</h6>
                                     <div className="form-group">
-                                        <input type="date" className="form-control form-control-lg" value = {currentProject.startDate} name="start_date" />
+                                        <input type="date" className="form-control form-control-lg" 
+                                        name="start_date"  onChange = {this.onChange}  value = {this.state.start_date} />
                                     </div>
                                     <h6>Estimated End Date</h6>
                                     <div className="form-group">
-                                        <input type="date" className="form-control form-control-lg" value = {currentProject.endDate} name="end_date" />
+                                        <input type="date" className="form-control form-control-lg" 
+                                        name="end_date"  onChange = {this.onChange} value = {this.state.end_date} />
                                     </div>
 
                                     <input type="submit" className="btn btn-primary btn-block mt-4" />
@@ -45,12 +79,12 @@ class UpdateProject extends Component {
     }
 }
 UpdateProject.propTypes = {
-    currentProject : PropTypes.object.isRequired
+    current_project : PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => {
     return {
-        currentProject : state.getProjectReducer.currentProject
+        current_project : state.getProjectReducer.currentProject
     }
 }
 
