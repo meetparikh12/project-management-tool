4ae23155d944c514980bbb5461dd180a31f1b85c
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-18 16:32:15 +0530

    Created Repository for Backlog Entity

---
+++ projectboard/src/main/java/com/meet/projectboard/repository/BacklogRepository.java
@@ -0,0 +1,11 @@
+package com.meet.projectboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.meet.projectboard.model.Backlog;
+
+@Repository 
+public interface BacklogRepository extends JpaRepository<Backlog, Long>{
+
+}
