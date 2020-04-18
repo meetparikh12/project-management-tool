0e9034769282ba1ca5a708a2f575715a108e3e15
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-18 20:21:27 +0530

    Modified [POST] method for creating project task

--- projectboard/src/main/java/com/meet/projectboard/controller/BacklogController.java
+++ projectboard/src/main/java/com/meet/projectboard/controller/BacklogController.java
@@ -1,8 +1,6 @@
 package com.meet.projectboard.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,30 +18,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meet.projectboard.model.ProjectTask;
+import com.meet.projectboard.service.MapValidationErrorService;
 import com.meet.projectboard.service.ProjectTaskService;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/projectboard")
-public class ProjectTaskController {
+@RequestMapping("/api")
+public class BacklogController {
 
 	@Autowired
 	private ProjectTaskService projectTaskService;
 	
-	@PostMapping("")
-	private ResponseEntity<?> addProjectTask(@Valid @RequestBody ProjectTask projectTask, BindingResult bindingResult){
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
+	@PostMapping("/backlog/{backlog_id}")
+	private ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult bindingResult, @PathVariable String backlog_id){
 		
-		if(bindingResult.hasErrors()) {
-			
-			Map<String, String> errorMap = new HashMap<String,String>();
-			
-			for(FieldError fieldError : bindingResult.getFieldErrors()) {
-				errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
-			}
-			return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(bindingResult);
+		if(errorMap != null) return errorMap;
 		
-		ProjectTask newProjectTask = projectTaskService.saveOrUpdateProjectTask(projectTask);
+		
+		ProjectTask newProjectTask = projectTaskService.addProjectTask(backlog_id, projectTask);
 		
 		return new ResponseEntity<ProjectTask>(newProjectTask,HttpStatus.OK);
 	}
--- projectboard/src/main/java/com/meet/projectboard/service/ProjectTaskService.java
+++ projectboard/src/main/java/com/meet/projectboard/service/ProjectTaskService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.meet.projectboard.model.Backlog;
 import com.meet.projectboard.model.ProjectTask;
+import com.meet.projectboard.repository.BacklogRepository;
 import com.meet.projectboard.repository.ProjectTaskRepository;
 
 @Service
@@ -13,16 +15,37 @@ public class ProjectTaskService {
 
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
+	@Autowired
+	private BacklogRepository backlogRepository;
 
-	public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+//	public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+//		
+//		if(projectTask.getStatus() == null || projectTask.getStatus() == "") {
+//			projectTask.setStatus("TO_DO");
+//		}
+//		projectTaskRepository.save(projectTask);
+//		return projectTask;
+//	}
+
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+		
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+		projectTask.setBacklog(backlog);
+		
+		Integer backlogSequence = backlog.getPTSequence();
+		backlogSequence++;
+		
+		projectTask.setProjectSequence(projectIdentifier + "-" +backlogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
 		
 		if(projectTask.getStatus() == null || projectTask.getStatus() == "") {
 			projectTask.setStatus("TO_DO");
 		}
-		projectTaskRepository.save(projectTask);
-		return projectTask;
+		
+		return projectTaskRepository.save(projectTask);
+		
 	}
-
+	
 	public List<ProjectTask> getAllProjectTasks() {
 		
 		return projectTaskRepository.findAll();
