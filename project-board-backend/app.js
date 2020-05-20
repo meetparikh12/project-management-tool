dadad27f27e504d5aac06bce11312b14e5c15c78
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-20 16:42:22 +0530

    Added user model and implemented register route

--- project-board-backend/app.js
+++ project-board-backend/app.js
@@ -3,10 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const ErrorHandling = require('./models/ErrorHandling');
 const UserRoutes = require('./routes/users');
+const ProjectRoutes = require('./routes/projects');
+const mongoose = require('mongoose');
+const {mongoURI} = require('./config/keys');
 
 app.use(bodyParser.json());
 
 app.use('/api/users', UserRoutes);
+app.use('/api/projects', ProjectRoutes);
 
 app.use('/', (req,res,next)=> {
     next(new ErrorHandling('Specified route does not exist', 404));
@@ -16,5 +20,12 @@ app.use((error,req,res,next)=> {
     res.status(error.status).json({message: error.message});
 })
 
-app.listen(4200);
-console.log('Server is listening on port 4200');
+mongoose.connect( mongoURI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=> {
+    app.listen(4200);
+    console.log('Server is listening on port 4200');
+}).catch((err)=> console.log(err));
--- project-board-backend/routes/users.js
+++ project-board-backend/routes/users.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const route = express.Router();
+const User = require('../models/User');
+const ErrorHandling = require('../models/ErrorHandling');
+const bcrypt = require('bcryptjs');
 
-route.get('/login', (req,res,next)=> {
-    res.status(200).json({message: 'Log in successfully!'})
+route.post('/register', async (req,res,next)=> {
+    const {name, email, password, confirmPassword }  =  req.body;
+    let user; 
+    try {
+        user = await User.findOne({email: email})
+    } catch(error) {
+        return next(new ErrorHandling('Try again!', 500));
+    } 
+    if(user) {
+        return next(new ErrorHandling('Email already exists', 422));        
+    }
+    if(password !== confirmPassword) {
+        return next(new ErrorHandling('Password does not match', 422));
+    }
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch(error) {
+        return next(new ErrorHandling('Password not hashed!', 500));
+    }
+
+    const newUser = new User({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    try {
+        await newUser.save();
+    } catch(error) {
+        return next(new ErrorHandling('User not registered!', 500));
+    }
+
+    res.status(201).json({user: newUser})
 })
 
 module.exports = route;
